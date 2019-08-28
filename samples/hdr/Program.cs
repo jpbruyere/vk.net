3c19e8a85fbeae514b809c43809fd019b34771f9
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2019-08-28 11:59:29 +0200

    debug resize in deferred

--- samples/hdr/Program.cs
+++ samples/hdr/Program.cs
@@ -13,42 +13,8 @@ namespace tests {
 			}
 		}
 
-		DebugReport dbgReport;
-		CVKL.DebugUtils.Messenger msg;
-
-		Image img;
 
 		Program () : base () {
-			msg = new CVKL.DebugUtils.Messenger (instance);
-
-			dbgReport = new DebugReport (instance,
-				VkDebugReportFlagsEXT.DebugEXT |
-				VkDebugReportFlagsEXT.ErrorEXT |
-				VkDebugReportFlagsEXT.WarningEXT |
-				VkDebugReportFlagsEXT.PerformanceWarningEXT |
-				VkDebugReportFlagsEXT.InformationEXT);
-
-
-			CommandBuffer cmd = cmdPool.AllocateAndStart (VkCommandBufferUsageFlags.OneTimeSubmit);
-
-			using (StbImage stbi = new StbImage ("data/textures/texture.jpg")) {
-
-				img = new Image (dev, VkFormat.R8g8b8a8Unorm, VkImageUsageFlags.TransferSrc | VkImageUsageFlags.TransferDst,
-					VkMemoryPropertyFlags.DeviceLocal, (uint)stbi.Width, (uint)stbi.Height, VkImageType.Image2D,
-					VkSampleCountFlags.SampleCount1, VkImageTiling.Optimal, Image.ComputeMipLevels (stbi.Width, stbi.Height));
-
-				using (HostBuffer stag = new HostBuffer (dev, VkBufferUsageFlags.TransferSrc, (ulong)stbi.Size, stbi.Handle)) {
-
-					stag.CopyTo (cmd, img, VkImageLayout.TransferSrcOptimal);
-					img.BuildMipmaps (cmd);
-
-					cmd.End ();
-					presentQueue.Submit (cmd);
-					presentQueue.WaitIdle ();
-					cmd.Free ();
-
-				}
-			}
 
 
 		}
@@ -57,7 +23,6 @@ namespace tests {
 		protected override void Dispose (bool disposing) {
 			if (disposing) {
 				if (!isDisposed) {
-					dbgReport?.Dispose ();
 				}
 			}
 			base.Dispose ();
