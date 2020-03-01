afbed9d0c6f958e06c3c410d44eb8c37733bc82c
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2020-03-01 17:13:50 +0100

    add vkEnumerateInstanceLayerProperties to preloaded funcs

--- VK/Version.cs
+++ VK/Version.cs
@@ -2,22 +2,20 @@
 {
     public struct Version
     {
-        private readonly uint value;
+        readonly uint value;
 
-        public Version(uint major, uint minor, uint patch)
-        {
+        public Version(uint major, uint minor, uint patch) {
             value = major << 22 | minor << 12 | patch;
         }
+		public Version (uint value) {
+			this.value = value;
+		}
 
-        public uint Major => value >> 22;
-
+		public uint Major => value >> 22;
         public uint Minor => (value >> 12) & 0x3ff;
-
         public uint Patch => (value >> 22) & 0xfff;
 
-        public static implicit operator uint(Version version)
-        {
-            return version.value;
-        }
-    }
+        public static implicit operator uint(Version version) => version.value;
+		public static implicit operator Version (uint version) => new Version(version);
+	}
 }
