9136c531af47890ab9aa6418dae053137a86f0ac
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2021-12-02 20:12:08 +0100

    tests vkcolorvalue

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
 using Vulkan;
 using static Vulkan.Vk;
 using Version = Vulkan.Version;
 
 namespace tests
 {
+	[StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Ansi)]
+	public struct NewStuff
+	{
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = (int)Vulkan.Vk.UuidSize)]
+		public StringBuilder name;
+
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 4*16)]
+		public float[,] calibrate;
+
+		[MarshalAs(UnmanagedType.R4)]
+		public float DMTI;
+
+		[MarshalAs(UnmanagedType.R4)]
+		public float DMTII;
+
+		// Etc
+	}
+
 	class Program
 	{
+
 		static void Main(string[] args)
 		{
 			VkInstance instance;
@@ -15,6 +37,10 @@ namespace tests
 			VkApplicationInfo ai = VkApplicationInfo.New();
 			//ai.apiVersion = new Version (1,0,0);
 			//ai.applicationVersion = new Version (1,0,0);
+
+
+			VkClearColorValue clear = new VkClearColorValue(255,0,0);
+			clear.float32.a = 210;
 
 			using (PinnedObjects pi = new PinnedObjects()) {
 				ci.pApplicationInfo = ai.Pin(pi);
